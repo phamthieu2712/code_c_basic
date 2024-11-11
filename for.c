2d1812539cb935b574e49c9a478d7fec70056f29
Pham thieu <pthieu2712@gmail.com>
2024-11-11 22:39:12 +0700

    use for while to printf even number

---
+++ for.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int i;
+    for (i = 1; i<=5; i++){
+        printf("cac so chan la:%d\n", i*2);
+    }
+    return 0;
+
+}
---
+++ while.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int i = 1;
+    while (i<=5){
+        printf("gia tri cac so chan la:%d\n", i*2);
+        i++;
+    }
+}
